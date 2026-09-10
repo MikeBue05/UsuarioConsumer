8263ac3dd0977d5338424ea68c429ce43499c67d
MikeBue05 <michaelbue04@gmail.com>
2026-09-10 16:06:25 -0300

    init: inicializa projeto

---
+++ src/main/java/org/example/usuarioconsumer/model/Usuario.java
@@ -0,0 +1,42 @@
+package org.example.usuarioconsumer.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+public class Usuario {
+    private Integer id;
+    private String cpf;
+    private String senha;
+
+    public Usuario() {}
+
+    public Usuario(Integer id, String cpf, String senha) {
+        this.id = id;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ src/main/java/service/UsuarioService.java
@@ -0,0 +1,17 @@
+package service;
+
+import org.example.usuarioconsumer.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private
+}
